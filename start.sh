f8b68c80a9f8855813465571c4ea306766e9c554
maullikpatell <115573338+maullikpatell@users.noreply.github.com>
2022-11-25 02:32:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,15 @@
-echo "Cloning Repo...."
-git clone https://github.com/RoyalKrrishna/MdiskWalaBot.git /MdiskWalaBot
-cd /MdiskWalaBot
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning Repo...."
+  git clone https://github.com/maullikpatell/MdiskSearchBot.git /MdiskSearchBot
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /MdiskSearchBot
+fi  
+cd /MdiskSearchBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
+
+
+
